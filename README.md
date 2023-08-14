2d186f4f78903494f3ce4c7490db6394eab9d439
Abhishek Dujari <abhishek.dujari@gmail.com>
2023-08-14 09:13:36 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# WAF Snap
+
+This snap can be used to analyze transactions
+and provide a score based on the results from [Wicked.us Anti-fraud](https://wicked.us)
+
+## Installation
+
+You can visit the site above for latest installation instructions.
+You can run this locally with the following command:
+
+```bash
+yarn install
+yarn mm-snap serve
+# or use
+yarn start
+```
---
+++ src/index.ts
@@ -0,0 +1,50 @@
+import { Json, OnTransactionHandler } from '@metamask/snaps-types';
+import { heading, panel, text } from '@metamask/snaps-ui';
+
+/**
+ * Analyze a transaction.
+ *
+ * @param transaction - The transaction to analyze.
+ * @param chainId - The chain ID of the transaction.
+ * @param origin - The origin of the transaction.
+ * @returns The result of the analysis.
+ */
+async function analyze(transaction: Json, chainId: string, origin?: string) {
+  const rawresponse = await fetch('http://127.0.0.1:5000/process', {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    method: 'POST',
+    body: JSON.stringify({
+      trx: transaction,
+      chainId,
+      origin,
+    }),
+  });
+
+  return rawresponse.json();
+}
+
+// Handle outgoing transactions.
+export const onTransaction: OnTransactionHandler = async ({
+  transaction,
+  chainId,
+  transactionOrigin,
+}) => {
+  const resp = await analyze(transaction, chainId, transactionOrigin);
+
+  // Return the result of the analysis.
+  return {
+    content: panel([
+      heading(`Transaction Risk - ${resp.result}`),
+      text(
+        `Address: ${resp.address.result} 
+         ${resp.address.is_contract ? '(Contract)' : ''} ${
+          resp.address.tags || ''
+        }`,
+      ),
+      text(`Blockchain: ${resp.chain.name} Info: ${resp.chain.result}`),
+      text(`App: ${resp.origin.result || ''} ${resp.origin.tags || ''}`),
+    ]),
+  };
+};
